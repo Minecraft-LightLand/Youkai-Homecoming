3ed4085c3d13c094a1172e6f9381cacf7e1dce94
Yuqi154 <1123489751@qq.com>
2024-06-15 15:49:35 +0800

    为东风谷早苗添加弹幕 神德「五谷丰穰米之浴」

--- src/main/java/dev/xkmc/youkaishomecoming/content/spell/game/sanae/Sugiruyoru.java
+++ src/main/java/dev/xkmc/youkaishomecoming/content/spell/game/sanae/Sugiruyoru.java
@@ -14,156 +14,288 @@ import net.minecraft.world.phys.Vec3;
 
 public class Sugiruyoru extends ActualSpellCard {
 
+    private int s = 0;
+
     @Override
     public void tick(CardHolder holder) {
         super.tick(holder);
-        if (tick % 10 == 0) {
-            //每10tick发射一次自机弹幕
-            if (holder.self() instanceof Mob mob && mob.getTarget() instanceof Frog) {
-                holder.shoot(holder.prepareDanmaku(50, holder.forward(), YHDanmaku.Bullet.BALL, DyeColor.RED));
-                //设置弹幕的属性——50tick的生存时间，前进方向为holder的前进方向，弹幕类型为CIRCLE，颜色为RED
-                return;
-            }
-            var le = holder.target();
-            // 获取目标实体
-            if (le == null) return;
-            var dir = holder.forward();
-            // 获取holder的前进方向
-            var ans = new Sugiruyoru.tardanmu();
-            // 创建一个StateChange对象
-            ans.pos = holder.center();
-            ans.init = dir;
-            ans.normal = dir;
 
-            addTicker(ans);
-            // 添加一个StateChange对象到当前的tickers中
+        if (tick % 20 == 0) {
+            var le = holder.target();
+            var pos = holder.center();
+            // 当目标实体和holder的中心在y轴上的距离小于10时
+            if (le == null || le.distanceTo(pos) < 25) {
+                s = 0;
+                // 设置s为0
+            }else {
+                s = 1;
+                // 设置s为1
+            }
         }
-        if (tick % 40 == 0) {
-            //每40tick发射一轮浮动弹幕
-            if (holder.self() instanceof Mob mob && mob.getTarget() instanceof Frog) {
-                holder.shoot(holder.prepareDanmaku(50, holder.forward(), YHDanmaku.Bullet.MENTOS, DyeColor.LIGHT_BLUE));
-                return;
-            }
-            var le = holder.target();
-            // 获取目标实体
-            if (le == null) return;
-            var dir = holder.forward();
-            // 获取holder的前进方向
-            var ans = new Sugiruyoru.floatdanmu();
-            // 创建一个StateChange对象
-            ans.pos = holder.center();
-            ans.init = dir;
-            ans.normal = dir;
 
-            addTicker(ans);
-            // 添加一个StateChange对象到当前的tickers中
+        switch (s) {
+            case 0:
+                //奇迹「客星辉煌之夜」
+                if (tick % 10 == 0) {
+                    //每10tick发射一次自机弹幕
+                    if (holder.self() instanceof Mob mob && mob.getTarget() instanceof Frog) {
+                        holder.shoot(holder.prepareDanmaku(80, holder.forward(), YHDanmaku.Bullet.BALL, DyeColor.RED));
+                        //设置弹幕的属性——50tick的生存时间，前进方向为holder的前进方向，弹幕类型为CIRCLE，颜色为RED
+                        return;
+                    }
+                    var le = holder.target();
+                    // 获取目标实体
+                    if (le == null) return;
+                    var dir = holder.forward();
+                    // 获取holder的前进方向
+                    var ans = new tardanmu1();
+                    // 创建一个StateChange对象
+                    ans.pos = holder.center();
+                    ans.init = dir;
+                    ans.normal = dir;
+                    addTicker(ans);
+                    // 添加一个StateChange对象到当前的tickers中
+                }
+                if (tick % 40 == 0) {
+                    //每40tick发射一轮浮动弹幕
+                    if (holder.self() instanceof Mob mob && mob.getTarget() instanceof Frog) {
+                        holder.shoot(holder.prepareDanmaku(60, holder.forward(), YHDanmaku.Bullet.MENTOS, DyeColor.LIGHT_BLUE));
+                        return;
+                    }
+                    var le = holder.target();
+                    // 获取目标实体
+                    if (le == null) return;
+                    var dir = holder.forward();
+                    // 获取holder的前进方向
+                    var ans = new floatdanmu1();
+                    // 创建一个StateChange对象
+                    ans.pos = holder.center();
+                    ans.init = dir;
+                    addTicker(ans);
+                    // 添加一个StateChange对象到当前的tickers中
+                }
+                break;
+            case 1:
+                //神德「五谷丰穰米之浴」
+                if (tick % 20 == 0) {
+                    //每10tick发射一次自机弹幕
+                    if (holder.self() instanceof Mob mob && mob.getTarget() instanceof Frog) {
+                        holder.shoot(holder.prepareDanmaku(80, holder.forward(), YHDanmaku.Bullet.BALL, DyeColor.RED));
+                        //设置弹幕的属性——50tick的生存时间，前进方向为holder的前进方向，弹幕类型为CIRCLE，颜色为RED
+                        return;
+                    }
+                    var le = holder.target();
+                    if (le == null) return;
+                    var dir = holder.forward();
+                    var ans = new tardanmu2();
+                    ans.pos = holder.center();
+                    ans.init = dir;
+                    ans.normal = dir;
+                    addTicker(ans);
+                }
+                if (tick % 2 == 0) {
+                    //每40tick发射一轮浮动弹幕
+                    if (holder.self() instanceof Mob mob && mob.getTarget() instanceof Frog) {
+                        holder.shoot(holder.prepareDanmaku(60, holder.forward(), YHDanmaku.Bullet.MENTOS, DyeColor.LIGHT_BLUE));
+                        return;
+                    }
+                    var le = holder.target();
+                    if (le == null) return;
+                    var dir = holder.forward();
+                    var ans = new floatdanmu2();
+                    ans.pos = holder.center();
+                    ans.init = dir;
+                    addTicker(ans);
+                }
         }
     }
 
-    public static class tardanmu extends Ticker<Sugiruyoru> {
+    //奇迹「客星辉煌之夜」
+    public static class tardanmu1 extends Ticker<Sugiruyoru> {
 
         @SerialClass.SerialField
         private Vec3 pos, init, normal;
 
         public boolean tick(CardHolder holder, Sugiruyoru card) {
             step(holder);
-            // 调用step方法
             super.tick(holder, card);
-            // 调用父类的tick方法
             return false;
         }
+
         private void step(CardHolder holder) {
             var le = holder.target();
-            // 获取目标实体
             if (le == null) return;
-            // 获取随机数
             if (init == null) {
                 pos = holder.center();
                 var dir = le.subtract(holder.center()).normalize();
-                init = DanmakuHelper.getOrientation(dir).rotateDegrees(0, 0);
-                normal = dir.cross(init);
+                init = dir;
+                normal = dir;
             }
             if (tick < 0) return;
             if (tick == 0) {
                 var o0 = DanmakuHelper.getOrientation(init, normal);
-                // 获取init和normal的方向
-                double acc = 16 * 2d / 50 / 50;
-                var front = o0.rotateDegrees(360.0 );
-                var vec = front.scale(acc * 50);
-                var e = holder.prepareDanmaku(50, vec, YHDanmaku.Bullet.CIRCLE, DyeColor.RED);
-                // 设置弹幕的属性
+                double acc = 16 * 2d / 80 / 80;
+                var front = o0.rotateDegrees(360.0);
+                var vec = front.scale(acc * 80 * 1.5);
+                var e = holder.prepareDanmaku(80, vec, YHDanmaku.Bullet.CIRCLE, DyeColor.RED);
                 e.mover = new RectMover(pos, vec, front.scale(-acc));
-                // 设置弹幕的移动器——RectMover（矩形移动器）
                 holder.shoot(e);
-                // 发射弹幕
-
             }
         }
     }
 
-    public static class floatdanmu extends Ticker<Sugiruyoru> {
+    //奇迹「客星辉煌之夜」
+    public static class floatdanmu1 extends Ticker<Sugiruyoru> {
         @SerialClass.SerialField
-        private Vec3 pos, init, normal;
+        private Vec3 pos, init;
 
         public boolean tick(CardHolder holder, Sugiruyoru card) {
             step(holder);
-            // 调用step方法
             super.tick(holder, card);
-            // 调用父类的tick方法
             return false;
         }
+
         private void step(CardHolder holder) {
             var le = holder.target();
-            // 获取目标实体
             if (le == null) return;
-            // 获取随机数
             if (init == null) {
                 pos = holder.center();
-                // 设置位置为holder的中心
-                var dir = le.subtract(holder.center()).normalize();
-                // 获取目标实体到holder中心的向量，并归一化
-                init = DanmakuHelper.getOrientation(dir).rotateDegrees(0, 0);
-                // 获取dir的方向，然后旋转90度
-                normal = dir.cross(init);
-                // 获取dir和init的叉乘
+                init = le.subtract(holder.center()).normalize();
             }
             if (tick < 0) return;
             if (tick % 2 == 0 && tick < 40) {
                 var dir = le.subtract(holder.center()).normalize();
                 var o0 = DanmakuHelper.getOrientation(dir);
-                // 获取init和normal的方向
                 for (int i = 0; i < 2; i++) {
                     var f0 = o0.rotateDegrees(180.0 * i + 90.0);
-                    // 获取o0旋转360/n*i度的方向
                     var p0 = pos.add(f0.scale(8));
-                    // 获取f0放大r0倍的向量
                     var f1 = le.subtract(p0).normalize();
-                    // 获取目标实体到p0的向量，并归一化
                     var o1 = DanmakuHelper.getOrientation(f1);
-                    // 获取f1的方向
                     var r2 = tick % 40;
-
-                    double acc = 10 * 2d / 50 / 50;
-
+                    double acc = 10 * 2d / 60 / 60;
                     var front = o1.rotateDegrees(360.0 * r2 / 40 + 90.0);
-
-                    var vec = front.scale(acc * 50);
-
-                    var e = holder.prepareDanmaku(40, vec, YHDanmaku.Bullet.MENTOS, DyeColor.LIGHT_BLUE);
-                    // 设置弹幕的属性
+                    var vec = front.scale(acc * 60 * 1.5);
+                    var e = holder.prepareDanmaku(60, vec, YHDanmaku.Bullet.MENTOS, DyeColor.LIGHT_BLUE);
                     e.setPos(p0);
-                    // 设置弹幕的位置
                     e.mover = new RectMover(p0, vec, Vec3.ZERO);
-                    // 设置弹幕的移动器
+                    holder.shoot(e);
+                }
+            }
+        }
+    }
+
+    //神德「五谷丰穰米之浴」
+    public static class tardanmu2 extends Ticker<Sugiruyoru> {
+
+        @SerialClass.SerialField
+        private Vec3 pos, init, normal;
+        @SerialClass.SerialField
+        private DanmakuHelper.Orientation o0;
+
+        public boolean tick(CardHolder holder, Sugiruyoru card) {
+            step(holder);
+            super.tick(holder, card);
+            return false;
+        }
+
+        private void step(CardHolder holder) {
+            var le = holder.target();
+            if (le == null) return;
+            if (init == null) {
+                pos = holder.center();
+                var dir = le.subtract(holder.center()).normalize();
+                init = dir;
+                normal = dir;
+            }
+            if (tick < 0) return;
+            if (tick == 0) {
+                var dir = le.subtract(holder.center()).normalize();
+                o0 = DanmakuHelper.getOrientation(dir);
+            }
+            if (tick % 2 == 0 && tick < 10) {
+                for (int i = 0; i < 5; i++) {
+                    var f0 = o0.rotateDegrees(90, 72 * i);
+                    var p0 = pos.add(f0.scale(12));
+                    var f1 = le.subtract(p0).normalize();
+                    var o1 = DanmakuHelper.getOrientation(f1);
+                    double acc = 36 * 2d / 80 / 80;
+                    var vec = f1.scale(acc * 80 * 1.5);
+                    var e = holder.prepareDanmaku(80, vec, YHDanmaku.Bullet.CIRCLE, DyeColor.RED);
+                    e.setPos(p0);
+                    e.mover = new RectMover(p0, vec, Vec3.ZERO);
+                    holder.shoot(e);
+                    for (int j = 0; j < 5; j++) {
+                        var front = o1.asNormal().rotateDegrees(72*j,60);
+                        vec = front.scale(acc * 80 );
+                        e = holder.prepareDanmaku(80, vec, YHDanmaku.Bullet.CIRCLE, DyeColor.RED);
+                        e.mover = new RectMover(p0, vec, Vec3.ZERO);
+                        holder.shoot(e);
+                    }
+                }
+            }
+        }
+    }
+
+    //神德「五谷丰穰米之浴」
+    public static class floatdanmu2 extends Ticker<Sugiruyoru> {
+        @SerialClass.SerialField
+        private Vec3 pos, init;
+
+        public boolean tick(CardHolder holder, Sugiruyoru card) {
+            step(holder);
+            super.tick(holder, card);
+            return false;
+        }
+
+        private void step(CardHolder holder) {
+            var le = holder.target();
+            if (le == null) return;
+            var vc = holder.targetVelocity();
+            if (vc == null) return;
+            le =le.add(vc.scale(20));
+            if (init == null) {
+                pos = holder.center();
+                init = le.subtract(holder.center()).normalize();
+            }
+            if (tick < 0) return;
+            if (tick == 0) {
+                var dir = le.subtract(holder.center()).normalize();
+                var o0 = DanmakuHelper.getOrientation(dir);
+                var r = holder.random();
+                for (int i = 0; i < 5; i++) {
+                    var f0 = o0.rotateDegrees(90, 72 * i);
+                    var p0 = pos.add(f0.scale(20));
+                    var f1 = le.subtract(p0).normalize();
+                    var o1 = DanmakuHelper.getOrientation(f1);
+                    double acc = 24 * 2d / 40 / 40;
+                    var front = o1.rotateDegrees(r.nextDouble() * 90.0 - 45.0, r.nextDouble() * 90.0 - 45.0);
+                    var vec = front.scale(acc * 40);
+                    var e = holder.prepareDanmaku(60, vec, YHDanmaku.Bullet.MENTOS, switchcolor(i));
+                    e.setPos(p0);
+                    e.mover = new RectMover(p0, vec, Vec3.ZERO);
                     holder.shoot(e);
 
                 }
             }
 
         }
+
+        private DyeColor switchcolor(int i){
+            switch (i){
+                case 0:
+                    return DyeColor.LIGHT_BLUE;
+                case 1:
+                    return DyeColor.GREEN;
+                case 2:
+                    return DyeColor.WHITE;
+                case 3:
+                    return DyeColor.YELLOW;
+                case 4:
+                    return DyeColor.RED;
+                default:
+                    return DyeColor.LIGHT_BLUE;
+            }
+        }
     }
-
-
-
 
 }
