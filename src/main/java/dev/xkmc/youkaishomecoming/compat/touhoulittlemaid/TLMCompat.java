7c124293b46dbe5925eed3b7f891d11eedcf6cd2
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-03-06 10:34:05 +0800

    ysm

--- src/main/java/dev/xkmc/youkaishomecoming/compat/touhoulittlemaid/TLMCompat.java
+++ src/main/java/dev/xkmc/youkaishomecoming/compat/touhoulittlemaid/TLMCompat.java
@@ -1,5 +1,6 @@
 package dev.xkmc.youkaishomecoming.compat.touhoulittlemaid;
 
+import com.github.tartaricacid.touhoulittlemaid.compat.ysm.YsmCompat;
 import com.github.tartaricacid.touhoulittlemaid.item.ItemGarageKit;
 import dev.xkmc.youkaishomecoming.content.entity.youkai.GeneralYoukaiEntity;
 import dev.xkmc.youkaishomecoming.content.spell.game.TouhouSpellCards;
@@ -14,9 +15,15 @@ public class TLMCompat {
 	public static void onInteract(PlayerInteractEvent.EntityInteract event) {
 		if (event.getTarget() instanceof GeneralYoukaiEntity e && event.getEntity().isCreative()) {
 			if (event.getItemStack().getItem() instanceof ItemGarageKit) {
+				var data = ItemGarageKit.getMaidData(event.getItemStack());
 				if (!event.getTarget().level().isClientSide()) {
-					String id = ItemGarageKit.getMaidData(event.getItemStack()).getString("ModelId");
-					TouhouSpellCards.setSpell(e, id);
+					var ysm = YsmCompat.getYsmMaidInfo(data);
+					if (ysm.isYsmModel()) {
+						TouhouSpellCards.setSpell(e, "{YSM}" + data);
+					} else {
+						String id = data.getString("ModelId");
+						TouhouSpellCards.setSpell(e, id);
+					}
 				}
 				event.setCancellationResult(InteractionResult.SUCCESS);
 				event.setCanceled(true);
--- src/main/java/dev/xkmc/youkaishomecoming/compat/touhoulittlemaid/TLMRenderHandler.java
+++ src/main/java/dev/xkmc/youkaishomecoming/compat/touhoulittlemaid/TLMRenderHandler.java
@@ -3,10 +3,14 @@ package dev.xkmc.youkaishomecoming.compat.touhoulittlemaid;
 import com.github.tartaricacid.touhoulittlemaid.api.entity.IMaid;
 import com.github.tartaricacid.touhoulittlemaid.api.event.ConvertMaidEvent;
 import com.github.tartaricacid.touhoulittlemaid.client.renderer.entity.EntityMaidRenderer;
+import com.github.tartaricacid.touhoulittlemaid.compat.ysm.YsmCompat;
+import com.github.tartaricacid.touhoulittlemaid.inventory.tooltip.YsmMaidInfo;
 import com.mojang.blaze3d.vertex.PoseStack;
 import dev.xkmc.fastprojectileapi.spellcircle.SpellCircleLayer;
 import dev.xkmc.youkaishomecoming.content.entity.youkai.GeneralYoukaiEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.nbt.TagParser;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Mob;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -44,7 +48,43 @@ public class TLMRenderHandler {
 			return mob;
 		}
 
-	}
+		@Override
+		public boolean isYsmModel() {
+			return mob.getModelId().startsWith("{YSM}");
+		}
 
+		private YsmMaidInfo getInfo() {
+			if (!isYsmModel()) return YsmMaidInfo.EMPTY;
+			try {
+				var tag = TagParser.parseTag(mob.getModelId().substring(5));
+				return YsmCompat.getYsmMaidInfo(tag);
+			} catch (Throwable ignored) {
+
+			}
+			return YsmMaidInfo.EMPTY;
+		}
+
+		@Override
+		public String getYsmModelId() {
+			var info = getInfo();
+			if (!info.isYsmModel()) return "";
+			return info.modelId();
+		}
+
+		@Override
+		public String getYsmModelTexture() {
+			var info = getInfo();
+			if (!info.isYsmModel()) return "";
+			return info.textureId();
+		}
+
+		@Override
+		public Component getYsmModelName() {
+			var info = getInfo();
+			if (!info.isYsmModel()) return Component.empty();
+			return info.name();
+		}
+
+	}
 
 }
