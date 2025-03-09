7e69e0a420bf25b663ac9b616039c56cb2428324
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-03-10 07:55:14 +0800

    texture update

--- src/test/java/gen/RackTest.java
+++ src/test/java/gen/RackTest.java
@@ -3,12 +3,19 @@ package gen;
 public class RackTest {
 
 	public static void main(String[] args) {
-		double total = 1;
-		int n = 14;
+		test(20, 2);
+		test(20, 6);
+		test(20, 10);
+		test(20, 14);
+		test(20, 18);
+	}
+
+	private static void test(int max, int n) {
+		double total = max;
 		double[] heat = new double[n];
 		for (int i = 0; i < n; i++) {
-			heat[i] += total * 0.05;
-			total *= 0.95;
+			heat[i] += total * 0.04;
+			total *= 0.96;
 		}
 		for (int i = n - 1; i >= 0; i--) {
 			heat[i] += total * 0.2;
@@ -16,11 +23,12 @@ public class RackTest {
 		}
 		StringBuilder sb = new StringBuilder("Racks: ");
 		for (int i = 0; i < n; i++) {
-			sb.append((int) Math.ceil(20 / heat[i]));
+			sb.append((int) Math.ceil(40 / heat[i]));
 			if (i < n - 1) sb.append(", ");
 		}
+		System.out.println("Layer: " + n);
 		System.out.println(sb);
-		System.out.println("Remain: " + Math.round(total * 10000) / 100d);
+		System.out.println("Remain: " + Math.round(total * 10000d / max) / 100d + "%");
 	}
 
 }
