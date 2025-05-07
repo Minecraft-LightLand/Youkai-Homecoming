f8bfdd56efc385a91d3866c9b2849f1339647a98
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-05-07 20:00:34 +0800

    fix leak

--- src/main/java/dev/xkmc/fastprojectileapi/render/core/BulkDataWriter.java
+++ src/main/java/dev/xkmc/fastprojectileapi/render/core/BulkDataWriter.java
@@ -1,26 +1,21 @@
 package dev.xkmc.fastprojectileapi.render.core;
 
-import com.mojang.blaze3d.platform.MemoryTracker;
 import com.mojang.blaze3d.vertex.BufferBuilder;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import dev.xkmc.youkaishomecoming.mixin.BufferBuilderAccessor;
 import net.minecraft.util.FastColor;
 import org.joml.Matrix4f;
 import org.joml.Vector4f;
-
-import java.nio.ByteBuffer;
 
 public class BulkDataWriter {
 
 	private final VertexConsumer vc;
 	private final BufferBuilder direct;
-	private final ByteBuffer buffer;
 
 	public BulkDataWriter(VertexConsumer vc, int size) {
 		this.vc = vc;
 		direct = vc instanceof BufferBuilder buf ? buf : null;
-		buffer = direct == null ? null : MemoryTracker.create(size * 6 * 4 * 4);
 	}
-
 
 	public void addVertex(Matrix4f m4, float x, float y, float z, float u, float v, int col) {
 		var vec = new Vector4f(x, y, z, 1).mul(m4);
@@ -28,27 +23,24 @@ public class BulkDataWriter {
 	}
 
 	public void addVertex(float x, float y, float z, float u, float v, int col) {
-		if (buffer == null) {
-			//vc.vertex(x, y, z).uv(u, v).color(col).endVertex();
-		} else if (true) {
-			buffer.putFloat(x);
-			buffer.putFloat(y);
-			buffer.putFloat(z);
-			buffer.putFloat(u);
-			buffer.putFloat(v);
-			buffer.put((byte) FastColor.ARGB32.red(col));
-			buffer.put((byte) FastColor.ARGB32.green(col));
-			buffer.put((byte) FastColor.ARGB32.blue(col));
-			buffer.put((byte) FastColor.ARGB32.alpha(col));
+		if (direct == null) {
+			vc.vertex(x, y, z).uv(u, v).color(col).endVertex();
+		} else {
+			direct.putFloat(0, x);
+			direct.putFloat(4, y);
+			direct.putFloat(8, z);
+			direct.putFloat(12, u);
+			direct.putFloat(16, v);
+			direct.putByte(20, (byte) FastColor.ARGB32.red(col));
+			direct.putByte(21, (byte) FastColor.ARGB32.green(col));
+			direct.putByte(22, (byte) FastColor.ARGB32.blue(col));
+			direct.putByte(23, (byte) FastColor.ARGB32.alpha(col));
+			((BufferBuilderAccessor) direct).setNextElementByte(((BufferBuilderAccessor) direct).getNextElementByte() + 24);
+			((BufferBuilderAccessor) direct).setVertices(((BufferBuilderAccessor) direct).getVertices() + 1);
 		}
 	}
 
-
 	public void flush() {
-		if (direct != null && buffer != null) {
-			buffer.position(0);
-			direct.putBulkData(buffer);
-		}
 	}
 
 }
