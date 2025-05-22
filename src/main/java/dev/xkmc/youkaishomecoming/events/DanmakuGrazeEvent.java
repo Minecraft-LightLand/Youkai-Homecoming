963f95d6b233ddff41c7666299e86d5ab1409bfd
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-05-22 11:41:02 +0800

    invul graze fix

--- src/main/java/dev/xkmc/youkaishomecoming/events/DanmakuGrazeEvent.java
+++ src/main/java/dev/xkmc/youkaishomecoming/events/DanmakuGrazeEvent.java
@@ -3,7 +3,9 @@ package dev.xkmc.youkaishomecoming.events;
 import dev.xkmc.fastprojectileapi.entity.GrazingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.Cancelable;
 
+@Cancelable
 public class DanmakuGrazeEvent extends PlayerEvent {
 
 	private final GrazingEntity e;
