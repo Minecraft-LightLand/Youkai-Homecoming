d5d21f424d89ab71051bc11059d364bffe0a7368
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-06-29 17:03:04 +0800

    basin shape

--- src/main/java/dev/xkmc/youkaishomecoming/content/pot/basin/BasinBlock.java
+++ src/main/java/dev/xkmc/youkaishomecoming/content/pot/basin/BasinBlock.java
@@ -5,19 +5,39 @@ import com.tterrag.registrate.providers.RegistrateBlockstateProvider;
 import dev.xkmc.l2modularblock.DelegateBlock;
 import dev.xkmc.l2modularblock.impl.BlockEntityBlockMethodImpl;
 import dev.xkmc.l2modularblock.mult.FallOnBlockMethod;
+import dev.xkmc.l2modularblock.one.ShapeBlockMethod;
 import dev.xkmc.l2modularblock.type.BlockMethod;
 import dev.xkmc.youkaishomecoming.content.pot.ferment.FermentationTankBlockEntity;
 import dev.xkmc.youkaishomecoming.init.registrate.YHBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.client.model.generators.ModelFile;
+import org.jetbrains.annotations.Nullable;
 
-public class BasinBlock implements FallOnBlockMethod {
+public class BasinBlock implements FallOnBlockMethod, ShapeBlockMethod {
 
 	public static final BlockMethod TE = new BlockEntityBlockMethodImpl<>(YHBlocks.FERMENT_BE, FermentationTankBlockEntity.class);
+
+	private static final VoxelShape SHAPE;
+
+	static {
+		var out = Block.box(0, 0, 0, 16, 9, 16);
+		var in = Block.box(1, 1, 1, 15, 9, 15);
+		SHAPE = Shapes.join(out, in, BooleanOp.FIRST);
+	}
+
+	@Override
+	public @Nullable VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext ctx) {
+		return SHAPE;
+	}
 
 	@Override
 	public boolean fallOn(Level level, BlockState state, BlockPos pos, Entity entity, float v) {
