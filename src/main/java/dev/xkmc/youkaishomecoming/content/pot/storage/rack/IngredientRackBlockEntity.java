9b824a7a02c35f89836b116bd802e607f76ed935
lcy0x1 <chengyilin@umail.ucsb.edu>
2025-07-16 19:46:49 +0800

    shift take all

--- src/main/java/dev/xkmc/youkaishomecoming/content/pot/storage/rack/IngredientRackBlockEntity.java
+++ src/main/java/dev/xkmc/youkaishomecoming/content/pot/storage/rack/IngredientRackBlockEntity.java
@@ -45,7 +45,8 @@ public class IngredientRackBlockEntity extends BaseBlockEntity implements BaseCo
 			if (ans.isEmpty()) return false;
 			if (hand == InteractionHand.OFF_HAND) return false;
 			if (!level.isClientSide()) {
-				player.setItemInHand(hand, ans.split(1));
+				int amount = player.isShiftKeyDown() ? Math.min(ans.getCount(), ans.getMaxStackSize()) : 1;
+				player.setItemInHand(hand, ans.split(amount));
 				level.playSound(null, getBlockPos(), SoundEvents.ITEM_PICKUP, SoundSource.BLOCKS, 1, 1);
 				notifyTile();
 			}
